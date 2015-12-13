21fc14e74bea91764b522dc95fea7d44bb2ad097
MagnusMogens <MagnusMogens@outlook.de>
2015-12-13 22:12:56 +0100

    Extend web site
    Add more documentation. Show the test center application.

--- js/main.js
+++ js/main.js
@@ -6,8 +6,28 @@ app.config(function($routeProvider) {
 			templateUrl: 'partials/home.html',
 			controller: 'PageCtrl'
 		})
+		.when('/test-center', {
+			templateUrl: 'partials/testcenter.html',
+			controller: 'PageCtrl'
+		})
 		.when('/doc/gameclient', {
 			templateUrl: 'partials/doc/gameclient.html',
+			controller: 'PageCtrl'
+		})
+		.when('/doc/session-manager', {
+			templateUrl: 'partials/doc/sessionmanager.html',
+			controller: 'PageCtrl'
+		})
+		.when('/doc/trophy', {
+			templateUrl: 'partials/doc/trophy.html',
+			controller: 'PageCtrl'
+		})
+		.when('/doc/data-storage', {
+			templateUrl: 'partials/doc/datastorage.html',
+			controller: 'PageCtrl'
+		})
+		.when('/doc/score', {
+			templateUrl: 'partials/doc/score.html',
 			controller: 'PageCtrl'
 		})
 		.otherwise('/404', {
